518f80686ff8df9746cdcf4c03f8b64b7fab7ac9
toscane <toscane.carro@edu.ece.fr>
2023-12-15 10:19:27 +0100

    hhh

--- accueil.php
+++ accueil.php
@@ -1,3 +1,6 @@
+<?php $connexion = new PDO("mysql:host=localhost;dbname=wd-projet", 'root'); ?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -6,14 +9,64 @@
     <link href="accueil.css" type="text/css" rel="stylesheet">
 </head>
 
+
+<h1>To-Do List</h1>
+    <button onclick="window.location.href = 'http://localhost/projetWD/projet_WB.php';" class="changement">Ajouter une tâche</button>
+    <button onclick="window.location.href = 'http://localhost/projetWD/edition_tache.php';" class="changement">Modifier une tâche</button>
+    <button onclick="window.location.href = 'http://localhost/projetWD/delete_tache.php';" class="changement">Supprimer une tâche</button>
+
+
+
+
 <body>
-    <div class="container">
-        <h1>To-Do List</h1>
-        <div class="buttons-container">
-            <button onclick="window.location.href = 'http://localhost/projetWD/projet_WB.php';" class="changement">Ajouter une tâche</button>
-            <button onclick="window.location.href = 'http://localhost/projetWD/edition_tache.php';" class="changement">Modifier une tâche</button>
-            <button onclick="window.location.href = 'http://localhost/projetWD/delete_tache.php';" class="changement">Supprimer une tâche</button>
-        </div>
-    </div>
-</body>
+<div class="container">
+    <form action="utilisateur.php" method="post">
+      <fieldset>
+        <p>
+          <option value="">Créer un utilisateur</option>
+          <fieldset>
+                <p>
+                    <label for="titre">Utilisateur :</label>
+                    <input type="text" name="uti" required="required" autofocus id="uti" placeholder="Alexandre" />
+                    <span id="Nom manquant"></span>
+                </p>
+          </fieldset>
+          <input type="submit" id ="submit" value="Ajouter" />
+        </p>
+    </form>
+  </div>
+
+  <div class="container">
+    <form action="utilisateur.php" method="post">
+      <fieldset>
+        <p>
+          <select id="utilisateur-select" name="utilisateur-select">
+          <option value="">Sélectionner un utilisateur</option>
+          <?php
+            $requete = $connexion->prepare("SELECT ID_Uti, Utilisateur FROM utilisateur");
+            $requete->execute();
+            while ($ligne = $requete->fetch(PDO::FETCH_ASSOC)) {
+              echo '<option value="' . $ligne['ID_Uti'] . '">' . $ligne['Utilisateur']. '</option>';
+            }
+          ?>
+          </select>
+        </p>
+        <input type="submit" id ="submit" value="Ajouter" />
+      </fieldset>
+    </form>
+  </div>
+
+  </body>
 </html>
+
+
+<script>
+        function validateForm() {
+            var utilisateur = document.getElementById("uti").value.trim();
+            if (utilisateur === "") {
+                document.getElementById("Nom_manquant").innerText = "Veuillez saisir un nom d'utilisateur.";
+                return false;
+            }
+            return true;
+        }
+    </script>
